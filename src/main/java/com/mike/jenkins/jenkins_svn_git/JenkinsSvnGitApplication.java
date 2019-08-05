f9ddf2d9b732429ac8762e6b03a1f40988f22ea9
Michael <2576731178@qq.com>
2019-08-06 02:15:54 +0800

    项目更改为war包

--- src/main/java/com/mike/jenkins/jenkins_svn_git/JenkinsSvnGitApplication.java
+++ src/main/java/com/mike/jenkins/jenkins_svn_git/JenkinsSvnGitApplication.java
@@ -2,12 +2,18 @@ package com.mike.jenkins.jenkins_svn_git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsSvnGitApplication {
+public class JenkinsSvnGitApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsSvnGitApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsSvnGitApplication.class);
+    }
 }
