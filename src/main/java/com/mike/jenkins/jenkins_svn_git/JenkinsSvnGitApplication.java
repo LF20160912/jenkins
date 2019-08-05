1ef519985cf553d16419a3440af2f36937ce1ec6
Michael <2576731178@qq.com>
2019-08-06 01:44:01 +0800

    上传项目

---
+++ src/main/java/com/mike/jenkins/jenkins_svn_git/JenkinsSvnGitApplication.java
@@ -0,0 +1,13 @@
+package com.mike.jenkins.jenkins_svn_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSvnGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSvnGitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mike/jenkins/jenkins_svn_git/controller/testcontroller.java
@@ -0,0 +1,17 @@
+package com.mike.jenkins.jenkins_svn_git.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class testcontroller {
+
+    @GetMapping
+    public  String sayHello()
+    {
+        return  "Hello Jenkins";
+    }
+}
